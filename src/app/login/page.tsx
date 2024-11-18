92844ae13280620efd250c9ded16d652469e232c
arya <aryapawar92@gmail.com>
2024-11-18 23:29:46 +0530

    file management done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-center">Hello From Arya</h1>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+function page() {
+  const [user, setUser] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="bg-grey-lighter min-h-screen flex flex-col bg-black">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+          <input
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="fullname"
+            placeholder="Full Name"
+          />
+
+          <input
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="email"
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="confirm_password"
+            placeholder="Confirm Password"
+          />
+
+          <button
+            type="submit"
+            className="w-full text-center py-3 rounded bg-black text-white hover:bg-green-dark focus:outline-none my-1"
+          >
+            Create Account
+          </button>
+        </div>
+
+        <div className="text-white mt-6">
+          Already have an account?
+          <a
+            className="no-underline border-b border-blue text-white"
+            href="../login/"
+          >
+            Log in
+          </a>
+          .
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
